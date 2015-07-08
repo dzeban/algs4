9b9ca7149d66e4d85ea3b6534b9075f3a44e80ae
Alex Dzyoba <avd@reduct.ru>
2015-07-08 21:13:11 +0300

    Percolation and PercolationStats implementation.

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -1,9 +1,13 @@
-import QuickUnionUF;
-
 public class Percolation {
     // Grid size
     private int N;
 
+    // Grid itself
+    private Site[] grid;
+
+    // Union-Find object to 
+    // calculate percolation
+    private WeightedQuickUnionUF unionFind;
 
     // Create N-by-N grid with all sites blocked
     public Percolation(int N) 
@@ -12,13 +16,65 @@ public class Percolation {
             throw new IllegalArgumentException("N must be positive");
 
         this.N = N;
+
+        // Initialize grid of sites
+        grid = new Site[N * N];
+        unionFind = new WeightedQuickUnionUF(N * N);
+        int i = 1;
+        int j = 1;
+        for (int n = 0; n < N * N; n++) 
+        {
+            grid[n] = new Site(i, j, n);
+
+            i = (i + 1) % (N + 1);
+
+            // Next row
+            if (i == 0) {
+                i = 1;
+                j++;
+            }
+        }
+    }
+
+    // Get site number in array as n = j*N + i, but
+    // coordinates are 1 based, while numbers are 0-based
+    private int coordinates2number(int i, int j)
+    {
+        return (j - 1) * N + (i - 1);
+    }
+
+    private Site getSite(int i, int j)
+    {
+        if (i < 1 || i > N || j < 1 || j > N)
+            return null;
+
+        int n = coordinates2number(i, j);
+        return grid[n];
+    }
+
+    private Site getSite(int n)
+    {
+        if (n < 0 || n > N * N)
+            return null;
+
+        return grid[n];
     }
 
     // Open site (row i, column j) if it is not open already
+    // i and j is "1" based
     public void open(int i , int j)
     {
-        if (i < 1 || i > N)
+        if (i < 1 || i > N || j < 1 || j > N)
             throw new IllegalArgumentException("Site must be (1..N, 1..N)");
+
+        Site site = getSite(i, j);
+        site.open();
+
+        // Connect to neighbors
+        site.connectTo(getSite(i-1, j), unionFind);
+        site.connectTo(getSite(i+1, j), unionFind);
+        site.connectTo(getSite(i, j-1), unionFind);
+        site.connectTo(getSite(i, j+1), unionFind);
     }
 
     // Is site (row i, column j) open?
@@ -26,6 +82,21 @@ public class Percolation {
     {
         if (i < 1 || i > N)
             throw new IllegalArgumentException("Site must be (1..N, 1..N)");
+
+        Site site = getSite(i, j);
+        return site.getOpen();
+    }
+
+    private boolean checkFull(Site site)
+    {
+        Site topSite;
+
+        for (int column = 1; column <= N; column++)
+        {
+            topSite = getSite(1, column);
+            if (unionFind.connected(site.getNumber(), topSite.getNumber()))
+                return true;
+        }
 
         return false;
     }
@@ -36,17 +107,91 @@ public class Percolation {
         if (i < 1 || i > N)
             throw new IllegalArgumentException("Site must be (1..N, 1..N)");
 
-        return false;
+        Site site = getSite(i, j);
+        return checkFull(site);
+    }
+
+    public boolean isFull(int number)
+    {
+        if (number < 0 || number > N*N)
+            throw new IllegalArgumentException("Site number must be (0..N*N)");
+
+        Site site = getSite(number);
+        return checkFull(site);
     }
 
     // Does the whole system percolates?
     public boolean percolates()
     {
+        Site bottomSite;
+        for (int column = 1; column <= N; column++)
+        {
+            bottomSite = getSite(N, column);
+            if (checkFull(bottomSite))
+                return true;
+        }
         return false;
     }
 
     // Test client
     public static void main(String [] args)
     {
+       Percolation pc = new Percolation(5);
+       //pc.open(0, 0);
+       pc.open(1, 1);
+       pc.open(2, 1);
+       pc.open(3, 3);
+       pc.open(5, 2);
+       pc.open(4, 2);
+       pc.open(3, 1);
+       pc.open(4, 1);
+       System.out.println(pc.isFull(3, 3));
+       System.out.println(pc.isFull(4, 2));
+       System.out.println(pc.percolates());
+    }
+
+    private class Site
+    {
+        // Site coordinates on a grid
+        private int i, j; 
+
+        // Site number in a grid
+        private int number; 
+
+        private boolean isOpen;
+
+        Site(int i, int j, int n)
+        {
+            this.i = i;
+            this.j = j;
+            this.number = n;
+            this.isOpen = false;
+
+            //System.out.printf("New site(%d, %d, %d)\n", i, j , n);
+        }
+
+        public int getNumber()
+        {
+            return number;
+        }
+
+        public void open()
+        {
+            isOpen = true;
+        }
+
+        public boolean getOpen()
+        {
+            return isOpen;
+        }
+
+        public void connectTo(Site neighbor, WeightedQuickUnionUF uf)
+        {
+            if (neighbor == null)
+                return;
+
+            if (neighbor.getOpen())
+                uf.union(number, neighbor.getNumber());
+        }
     }
 }
--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -1,42 +1,90 @@
 public class PercolationStats {
 
+    private double[] results;
+    private int N;
+    private int T;
+
     // Perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T) 
     {
         if (N <= 0 || T <= 0)
             throw new IllegalArgumentException("N and T must be at least 1");
+
+        this.N = N;
+        this.T = T;
+
+        results = new double[T];
+        for (int i = 0; i < T; i++)
+            results[i] = doExperiment(N);
+    }
+
+    private double doExperiment(int N)
+    {
+        int i, j;
+        int openSites = 0;
+        Percolation pc = new Percolation(N);
+
+        do {
+            i = StdRandom.uniform(1, N + 1);
+            j = StdRandom.uniform(1, N + 1);
+            if (pc.isOpen(i, j))
+                continue;
+
+            pc.open(i, j);
+            openSites++;            
+        } while(!pc.percolates());
+
+        //StdOut.printf("openSites %d, N2 %d, res %f\n",
+                //openSites, N*N, (double)openSites/(N*N));
+        return (double)openSites / (N * N);
     }
 
     // sample mean of percolation threshold
     public double mean() 
     {
+        return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() 
     {
+        return StdStats.stddev(results);
     }
 
     // low  endpoint of 95% confidence interval
     public double confidenceLo()
     {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi()
     {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
     }
 
-    // test client (described below)
+    // test client
     public static void main(String[] args)
     {
+        if (args.length != 2) {
+            StdOut.println("Requires 2 arguments <N> (grid size) and <T> (iterations)");
+            System.exit(1);
+        }
+
         // Read 2 arguments - N and T.
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
 
         // Perform T independent computation experiments 
         // on N-by-N grid
+        PercolationStats pcs = new PercolationStats(N, T);
         
         // Print out the mean, standart deviation and 95% confidence interval
         // for the percolation threshold.
+        StdOut.printf("mean\t= %f\n", pcs.mean());
+        StdOut.printf("stddev\t= %f\n", pcs.stddev());
+        StdOut.printf("95%% confidence interval\t= %f %f\n", 
+                pcs.confidenceLo(), pcs.confidenceHi());
 
     }
 }
